fe8f8c320f7432b67d78eeb5be5dc37bc9bc3ab7
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-21 12:58:19 +0200

    ex6

---
+++ ex6/ex6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex6
+{
+    class Program
+    {   static void sir(int n)
+        {
+            int k;
+            for(int i = 1; i <= n; i++)
+            {
+                k = 1;
+                while (k <= i)
+                {
+                    for (int d = 1; d <= i; d++) Console.WriteLine(k + " ");
+                    k++;
+                }
+
+
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            sir(n);
+
+
+        }
+    }
+}
