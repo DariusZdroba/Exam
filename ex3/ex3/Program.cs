8515fa82478f806fc534c0f1f0bc9d9e477b13e3
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-21 12:35:30 +0200

    ex3

---
+++ ex3/ex3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {    static void lista()
+        {
+            int n;
+            int maxim1 = 0, maxim2 = 0, maxim3 = 0;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            maxim1 = n;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            if (n >= maxim1)
+            {
+                maxim2 = maxim1;
+                maxim1 = n;
+            }
+            else
+                maxim2 = n;
+                while (n != 0)
+            {
+                line = Console.ReadLine();
+                n = int.Parse(line);
+                if ((n > maxim1)&&(n!=0))
+                {
+                    maxim3 = maxim2;
+                    maxim2 = maxim1;
+                    maxim1 = n;
+                }
+               
+            }
+            Console.WriteLine(maxim1 + " " + maxim2 + " " + maxim3);
+
+        }   
+        static void Main(string[] args)
+        {
+
+            lista();
+        }
+    }
+}
