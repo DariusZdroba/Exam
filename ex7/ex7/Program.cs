c129de0c406dcdbba91bc1965198d8d9fbc32975
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-21 13:06:10 +0200

    ex7

---
+++ ex7/ex7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex7
+{
+    class Program
+       {
+            static int minPar(int n)
+        {
+            if (n > 9)
+            {
+                if (n % 2 != 0) return minPar(n / 10);
+
+                else if (n % 10 < minPar(n / 10) || minPar(n / 10) == -1) return n % 10;
+
+                else return minPar(n / 10);
+
+            }
+
+            else if (n % 2 == 0) return n;
+
+            else return -1;
+
+        }
+        static void Main(string[] args)
+        {
+
+          Console.WriteLine(minPar(1234));
+        }
+    }
+}
